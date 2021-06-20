94be70ad41380fb110af969c0e8697c73bca33cd
Jesús Pardos <jesus.pardos88@gmail.com>
2021-06-20 20:12:30 +0200

    create history chart

--- Python/app.py
+++ Python/app.py
@@ -4,18 +4,28 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import joblib
+from utils import chart_altair
 
-cols = ['day', 'year', 'month', 'system', 'Holiday', 'weekday', 'tmin', 'tmed', 'presMin', 'prec', 'sol', 'velmedia', 'tmax', 'presMax', 'racha']
+cols = ['day', 'year', 'month', 'system', 'Holiday', 'weekday', 'tmin','tmed', 'presMin', 'prec', 'sol', 'velmedia', 'tmax', 'presMax', 'racha']
+cols_renov= ['day', 'year', 'month', 'system', 'Holiday', 'weekday', 'tmin', 'presMin', 'prec', 'sol', 'velmedia', 'tmax', 'presMax', 'racha']
+
+
 features = pd.DataFrame(columns=cols)
-start_date = dt.date.today()
-st.title("Predict Electric Generation from weather")
 path_model = '../models/'
+path_data = '../Data/'
+
+st.set_page_config(page_title='Electric Generation',layout='wide')
+st.title("Predict Electric Generation from weather")
+
+
+
 
 with st.sidebar:
     st.title('Weather selection')
     with st.form(key='Weather form'):
         weather = {}
         err = 0
+        start_date = dt.date.today()
         start_date = st.date_input('Fecha de inicio', value=start_date)
         weather['system'] = st.selectbox('Ree System', ['peninsular', 'canarias', 'baleares', 'melilla'])
         date = str(start_date)
@@ -36,7 +46,7 @@ with st.sidebar:
             weather['Holiday'] = 1
             
         weather['presMax'] = st.slider('Max pressure:', min_value=850.0, max_value=1100.0, step=0.1, value=1000.0)
-        weather['presMin'] = st.slider('Min pressure:', min_value=850.0, max_value=1100.0, step=0.1, value=1000.0)
+        weather['presMin'] = st.slider('Min pressure:', min_value=850.0, max_value=1100.0, step=0.1, value=900.0)
         if weather['presMax'] <= weather['presMin']:
             st.error('Error: Max pressure have to be greater than Min pressure')
             err = 1
@@ -46,7 +56,7 @@ with st.sidebar:
         weather['tmed'] = st.slider('Average temperature:', min_value=-30.0, max_value=50.0, step=0.1, value=15.0)
         weather['tmin'] = st.slider('Min temperature:', min_value=-30.0, max_value=50.0, step=0.1, value=10.0)
         if weather['tmax'] <= weather['tmed']:
-            st.error('Error: Max temperature can´t be lower than average temperature')
+            st.error('Error: Max temperature can´t be lower than Average temperature')
             err = 1
         elif weather['tmed'] <= weather['tmin']:
             st.error('Error: Average temperature can´t be lower than min temperature')
@@ -69,59 +79,82 @@ if submit_button_predict and err == 0:
     features = features.append(weather, ignore_index=True)
 
     reg_renov = joblib.load(path_model+'best_model_renovable.sav')
-    perc_renov = reg_renov.predict(features)
+    perc_renov = reg_renov.predict(features[cols_renov])
         
     reg_generation = joblib.load(path_model+'best_model_Generation.sav')
     value_generation = reg_generation.predict(features)
 
     reg_tech = joblib.load(path_model+'best_model_tech.sav')
-    value_tech = reg_tech.predict(features)
-
+    value_tech = reg_tech.predict(features[cols_renov])
     value_renov = perc_renov*value_generation
     perc_norenov = 1-perc_renov
+    prec_norenov_str = str(round(perc_norenov[0] * 100, 2))
+    prec_renov_str = str(round(perc_renov[0]*100,2))
     value_norenov = value_generation-value_renov
-    perc_tech = value_tech
-    perc_other = perc_renov - perc_tech
-    st.write('---------------------------------------------')
-    st.write('History')
-    st.write('---------------------------------------------')
-    st.write('Generación Total:')
-    st.write(value_generation)
-    st.write('Porcentaje de energía renovable:')
-    st.write(perc_renov*100)
-    st.write('Valores de energía renovable y no renovable:')
-    st.write(value_renov)
-    st.write(value_norenov)
-    st.write('Porcentaje de energia Solar Fotovoltaica :')
-    st.write(perc_tech*100)
-    st.write('Valor de energia Solar Fotovoltaica :')
-    st.write(value_tech)
+    perc_tech1 = value_tech[:,0]
+    value_tech1 = perc_tech1*value_generation
+    perc_tech1_str = str(round(perc_tech1[0] * 100, 2))
+    perc_tech2 = value_tech[:,1]
+    value_tech2 = perc_tech2 * value_generation
+    perc_tech2_str = str(round(perc_tech2[0]*100,2))
+    perc_other = perc_renov - perc_tech1 - perc_tech2
+
+    if perc_other>0:
+        value_other = perc_other * value_generation
+        perc_tech_ot_str = str(round(perc_other[0]*100,2))
+    else:
+        value_other = [0]
+        perc_tech_ot_str = '0.00'
+        perc_other = 0
 
     st.write('---------------------------------------------')
 
-    plt.rcParams.update({'font.size': 8})
-    fig, ax = plt.subplots()
+    st.header('Predicted values')
+    st.write('\n')
 
-    size = .3
-    radius=.9
-    angle=90
-    vals = np.array([[perc_norenov,0], [perc_tech,perc_other]])
+    c1, c2 = st.beta_columns((4, 5))
+    with c2:
+        st.subheader('Explanation')
+        st.write('\n')
+        st.write('For '+str(start_date)+' in '+weather['system']+' system our model predict a total generation of '+str(round(value_generation[0],3))+' Mwh.')
+        st.write('That is divided into '+str(round(value_norenov[0],3))+' Mwh ('+prec_norenov_str+'%) no renewable energy ')
+        st.write('and '+str(round(value_renov[0],3))+' Mwh ('+prec_renov_str+'%) renewable energy.')
+        st.write('\n')
+        st.write('Renewal energy is distributed in:')
+        st.write('- Wind power:'+str(round(value_tech2[0],3))+ ' Mwh ('+perc_tech2_str+'%)')
+        st.write('- Solar photovoltaic: '+str(round(value_tech1[0],3))+' Mwh ('+perc_tech1_str+'%)')
+        st.write('- Others: '+str(round(value_other[0],3))+' Mwh ('+perc_tech_ot_str+'%)')
 
-    cmap = plt.get_cmap("tab20c")
-    outer_colors = cmap([5, 9])
-    inner_colors = cmap([6, 7, 10,11,12])
+    with c1:
+        st.write('\n')
+        plt.rcParams.update({'font.size': 9})
+        fig, ax = plt.subplots()
 
-    ax.pie(vals.sum(axis=1), radius=radius, colors=outer_colors,
-           labels=[str(round(perc_norenov[0]*100,2))+'% No Renewable ',str(round(perc_renov[0]*100,2))+'% Renewable'],
-           labeldistance=.8,startangle=angle,
-           wedgeprops=dict(width=size, edgecolor='w',linewidth=3))
+        size = .3
+        radius=.9
+        angle=45
+        vals = np.array([[perc_norenov,0,0], [perc_tech1,perc_tech2,perc_other]])
 
-    ax.pie(vals.flatten(), radius=radius - size, colors=inner_colors,
-           labels=['','',str(round(perc_tech[0]*100,2))+'% Solar',str(round(perc_other[0]*100,2))+'% Other'],
-           labeldistance=.45,startangle=angle,
-           wedgeprops=dict(width=size, edgecolor='w',linewidth=1.5))
+        cmap = plt.get_cmap("tab20c")
+        outer_colors = cmap([5, 9])
+        inner_colors = cmap([6, 7, 8, 9, 10, 11])
 
-    ax.set(aspect="equal", title='Distribution of predicted generation')
+        ax.pie(vals.sum(axis=1), radius=radius, colors=outer_colors,
+               labels=[prec_norenov_str+'% No Renewable ',prec_renov_str+'% Renewable'],
+               labeldistance=.8,startangle=angle,
+               wedgeprops=dict(width=size, edgecolor='w',linewidth=3))
 
+        ax.pie(vals.flatten(), radius=radius - size, colors=inner_colors,
+               labels=['','','',perc_tech1_str+'% Solar',perc_tech2_str+'% Wind',perc_tech_ot_str+'% Other'],
+               labeldistance=.5,startangle=angle,
+               wedgeprops=dict(width=size, edgecolor='w',linewidth=1.5))
 
-    st.pyplot(fig)
+        ax.set(aspect="equal", title='Distribution of predicted generation')
+
+        st.pyplot(fig)
+
+    st.write('---------------------------------------------')
+    st.header('History distribution of electric generation')
+    st.write("\n")
+    df_rolling = pd.read_pickle(path_data + 'Rolling_chart.pkl')
+    st.altair_chart(chart_altair(df_rolling))
--- Python/utils.py
+++ Python/utils.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import altair as alt
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
@@ -72,13 +73,14 @@ def target_preprocesing(df, targets_precentage, systems_names):
 
     systems_names.append('fecha')
 
-    df_group = df.groupby(systems_names,as_index=False).sum()
+    df_group = df.groupby(systems_names, as_index=False).sum()
 
     for target in targets_precentage:
       df_group[target] = (df_group[target]/df_group['Generacion_Mwh'])
     
     return df_group
     
+
 def date_transform(df):
 
   df['year'] = np.log(df['year'])
@@ -196,3 +198,118 @@ def plot_real_vs_pred(system, target, X_test, y_test, reg_ln, reg_KN, reg_DT, re
   sns.despine()
 
   return None
+
+
+def chart_altair(df, system_ini='peninsular'):
+
+    x_labels = ['2016-01-01', '2016-02-01', '2016-03-01', '2016-04-01', '2016-05-01', '2016-06-01',
+                '2016-07-01', '2016-08-01', '2016-09-01', '2016-10-01', '2016-11-01', '2016-12-01', '2016-12-31',
+                '2017-01-01', '2017-02-01', '2017-03-01', '2017-04-01', '2017-05-01', '2017-06-01',
+                '2017-07-01', '2017-08-01', '2017-09-01', '2017-10-01', '2017-11-01', '2017-12-01', '2017-12-31',
+                '2018-01-01', '2018-02-01', '2018-03-01', '2018-04-01', '2018-05-01', '2018-06-01',
+                '2018-07-01', '2018-08-01', '2018-09-01', '2018-10-01', '2018-11-01', '2018-12-01', '2018-12-31',
+                '2019-01-01', '2019-02-01', '2019-03-01', '2019-04-01', '2019-05-01', '2019-06-01',
+                '2019-07-01', '2019-08-01', '2019-09-01', '2019-10-01', '2019-11-01', '2019-12-01', '2019-12-31',
+                '2020-01-01', '2020-02-01', '2020-03-01', '2020-04-01', '2020-05-01', '2020-06-01',
+                '2020-07-01', '2020-08-01', '2020-09-01', '2020-10-01', '2020-11-01', '2020-12-01', '2020-12-31',
+                '2021-01-01', '2021-02-01', '2021-03-01', '2021-04-01', '2021-05-01', '2021-06-01',
+                '2021-07-01', '2021-08-01', '2021-09-01', '2021-10-01', '2021-11-01', '2021-12-01', '2021-12-31']
+
+    domain = ['Generación total', 'Renovable', 'Solar fotovoltaica', 'Eólica']
+    range_ = ['#85C1E9', '#239B56', '#D35400', '#F7DC6F']
+
+    select_box_sys = alt.binding_select(options=list(df['system'].unique()))
+
+    selection_sys = alt.selection_single(name='REE',
+                                         fields=['system'],
+                                         bind=select_box_sys,
+                                         init={'system': system_ini})
+
+    select_radio_year = alt.binding_radio(options=list(df['year'].unique()))
+
+    selection_year = alt.selection_single(name='Choose',
+                                          fields=['year'],
+                                          bind=select_radio_year,
+                                          init={'year': max(df['year'])})
+
+    nearest = alt.selection(type='single', nearest=True, on='mouseover',
+                            fields=['fecha'], empty='none')
+
+    selectors = alt.Chart(df).mark_point().encode(
+        alt.X('fecha'),
+        opacity=alt.value(0)
+    ).add_selection(
+        nearest
+    ).transform_filter(
+        selection_sys
+    ).transform_filter(
+        selection_year
+    )
+
+    bar = alt.Chart(df[df['Renov_norenov'] == 'Generación total']).mark_area(color='#85C1E9').encode(
+        alt.X('fecha', axis=alt.Axis(values=x_labels, labelAngle=0)),
+        alt.Y('Generacion_Mwh:Q')
+
+    ).add_selection(
+        selection_sys, selection_year
+    ).transform_filter(
+        selection_sys
+    ).transform_filter(
+        selection_year
+    ).properties(
+        width=1400,
+        height=450
+    )
+
+
+    bar_renov = alt.Chart(df[df['Tecnologia'] == 'Renovable']).mark_area().encode(
+        alt.X('fecha'),
+        alt.Y('Generacion_Mwh:Q'),
+        color=alt.Color('Tecnologia', scale=alt.Scale(domain=domain, range=range_))
+    ).transform_filter(
+        selection_sys
+    ).transform_filter(
+        selection_year
+    )
+
+    text_renov = bar_renov.mark_text(align='left', dx=3, dy=-20, color='#212F3C').encode(
+        text=alt.condition(nearest, 'Generacion_Mwh', alt.value(' '))
+    )
+
+    rules = alt.Chart(df).mark_rule(color='gray').encode(
+        x='fecha',
+    ).transform_filter(
+        nearest
+    )
+
+    bar_solar = alt.Chart(df[df['Tecnologia'] == 'Solar fotovoltaica']).mark_area(opacity=.8, color='#D35400').encode(
+        alt.X('fecha'),
+        alt.Y('Generacion_Mwh:Q')
+    ).transform_filter(
+        selection_sys
+    ).transform_filter(
+        selection_year
+    )
+
+    text_solar = bar_solar.mark_text(align='left', dx=5, dy=-5, color='#212F3C').encode(
+        text=alt.condition(nearest, 'Generacion_Mwh', alt.value(' '))
+    )
+
+    bar_eolica = alt.Chart(df[df['Tecnologia'] == 'Eólica']).mark_area(color='#F7DC6F').encode(
+        alt.X('fecha'),
+        alt.Y('Generacion_Mwh:Q')
+    ).transform_filter(
+        selection_sys
+    ).transform_filter(
+        selection_year
+    )
+
+    text_eolica = bar_eolica.mark_text(align='left', dx=5, dy=-5, color='#212F3C').encode(
+        text=alt.condition(nearest, 'Generacion_Mwh', alt.value(' '))
+    )
+
+    return alt.layer(bar, bar_renov, bar_eolica, bar_solar, selectors, rules, text_renov, text_eolica, text_solar
+                    ).configure_axis(labelFontSize=13,titleFontSize=14
+                    ).configure_text(fill='#212F3C', fontSize=13
+                    ).configure_legend(labelFontSize=14).interactive()
+
